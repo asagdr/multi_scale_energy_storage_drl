081d4c6fa43a02327340a9a35980ef420f7546a7
asagdr <wutianjunwu91@gmail.com>
2025-08-02 11:22:48 +0800

    Update __init__.py

--- tests/unit_tests/__init__.py
+++ tests/unit_tests/__init__.py
@@ -1,1 +1,11 @@
+"""
+单元测试模块
+"""
 
+from .test_battery_models import TestBatteryCellModel
+from .test_degradation import TestDegradationModel
+
+__all__ = [
+    'TestBatteryCellModel',
+    'TestDegradationModel'
+]
