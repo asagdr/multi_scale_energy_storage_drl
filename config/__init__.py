0f3097aaef36d92a24e577cea73b73f9405505a1
asagdr <wutianjunwu91@gmail.com>
2025-08-02 11:00:31 +0800

    Update __init__.py

--- config/__init__.py
+++ config/__init__.py
@@ -1,1 +1,23 @@
+"""
+配置模块
+包含系统的各种配置参数
+"""
 
+from .battery_params import BatteryParams
+from .system_config import SystemConfig
+from .training_config import TrainingConfig, UpperLayerConfig, LowerLayerConfig
+from .model_config import ModelConfig, ModelType
+from .hyperparameters import HyperParameters
+
+__all__ = [
+    'BatteryParams',
+    'SystemConfig', 
+    'TrainingConfig',
+    'UpperLayerConfig',
+    'LowerLayerConfig',
+    'ModelConfig',
+    'ModelType',
+    'HyperParameters'
+]
+
+__version__ = '0.1.0'
