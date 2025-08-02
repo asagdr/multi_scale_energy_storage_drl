b0199ca159beb97c75631c5dbef460aded996a04
asagdr <wutianjunwu91@gmail.com>
2025-08-02 11:13:39 +0800

    Update __init__.py

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,1 +1,27 @@
+"""
+环境模块
+包含储能系统环境和相关组件
+"""
 
+from .storage_environment import StorageEnvironment
+from .multi_scale_scheduler import MultiScaleScheduler, TimeScale, SchedulerMode
+from .constraint_validator import ConstraintValidator, ConstraintType, ViolationSeverity
+from .reward_calculator import RewardCalculator, RewardType
+from .state_manager import StateManager, StateScope, StateType
+
+__all__ = [
+    'StorageEnvironment',
+    'MultiScaleScheduler',
+    'TimeScale', 
+    'SchedulerMode',
+    'ConstraintValidator',
+    'ConstraintType',
+    'ViolationSeverity',
+    'RewardCalculator',
+    'RewardType',
+    'StateManager',
+    'StateScope',
+    'StateType'
+]
+
+__version__ = '0.1.0'
